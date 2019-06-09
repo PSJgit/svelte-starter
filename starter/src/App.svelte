65b5825861bb71bcf5b1dd68710d0fcb95b7452d
psjgit <psjj.dev@gmail.com>
2019-06-09 12:03:21 +1000

    svelte starter

---
+++ starter/src/App.svelte
@@ -0,0 +1,63 @@
+<script>
+  import ContactCard from "./ContactCard.svelte";
+
+  let name = "Max";
+  let title = "";
+  let image = "";
+  let description = "";
+  let formState = 'empty';
+
+  let createdContact;
+  const addContact = () => {
+    if (name.trim().length === 0 || title.trim().length === 0 || image.trim().length === 0 || description.trim().length === 0) {
+      formState = 'invalid';
+      return;
+    }
+    createdContact = {
+      name: name,
+      jobTitle: title,
+      imageUrl: image,
+      desc : description
+    }
+    formState = 'done';
+  };
+
+</script>
+
+<style>
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
+</style>
+
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+
+<button on:click={addContact}>Add Contact Card</button>
+
+{#if formState === 'done'}
+  <ContactCard 
+    userName={name} 
+    jobTitle={title}
+    {description} 
+    userImage={image} />
+{:else if formState === 'invalid'}
+  <p>Invalid input</p>
+{/if}
