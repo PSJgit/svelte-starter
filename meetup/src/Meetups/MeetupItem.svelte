cccd788ac145e52f9ea06b77855d479365c530de
Patrick Jones <patrick.jones@comparethemarket.com.au>
2019-07-29 16:02:18 +1000

    refactor logic after store

--- meetup/src/Meetups/MeetupItem.svelte
+++ meetup/src/Meetups/MeetupItem.svelte
@@ -1,5 +1,6 @@
 <script>
-  import { createEventDispatcher } from "svelte";
+  import meetups from './meetups-store.js';
+
   import Button from "../UI/Button.svelte";
   import Badge from "../UI/Badge.svelte";
 
@@ -12,7 +13,10 @@
   export let email;
   export let isFav;
 
-  const dispatch = createEventDispatcher();
+  function toggleFavorite() {
+
+    meetups.toggleFavorite(id);
+  }
 </script>
 
 <style>
@@ -96,7 +100,7 @@
       mode="outline"
       color={isFav ? null : 'success'}
       type="button"
-      on:click={() => dispatch('togglefavorite', id)}>
+      on:click={toggleFavorite}>
       {isFav ? 'Unfavorite' : 'Favorite'}
     </Button>
     <Button type="button">Show Details</Button>
